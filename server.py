4c92d8a4adc29a2cc9324b9529f25dd62400a12a
Stefan Gherman <scgheman@gmail.com>
2020-02-03 06:52:02 +0200

    Add image lists that are random.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
+import random
 
 app = Flask(__name__);
 
@@ -7,12 +8,31 @@ app = Flask(__name__);
 def return_index():
     return render_template("index.html");
 
-@app.route('/game', methods=['GET','POST'])
+
+@app.route('/game', methods=['GET', 'POST'])
 def return_game():
     if request.method == "POST":
         number_of_cards = request.form["exampleFormControlSelect1"]
-        print(number_of_cards)
-        return render_template("game.html", cards = int(number_of_cards))
+        arr_10 = ["fas fa-baby-carriage fa-5x", "fas fa-baby-carriage fa-5x", "fas fa-pegasus fa-5x",
+                  "fas fa-pegasus fa-5x",
+                  "fas fa-dragon fa-5x", "fas fa-dragon fa-5x", "fas fa-skull-cow fa-5x", "fas fa-skull-cow fa-5x",
+                  "fas fa-fighter-jet fa-5x",
+                  "fas fa-fighter-jet fa-5x"]
+
+        arr_20 = ["fas fa-baby-carriage fa-5x", "fas fa-baby-carriage fa-5x", "fas fa-pegasus fa-5x",
+                  "fas fa-pegasus fa-5x",
+                  "fas fa-dragon fa-5x", "fas fa-dragon fa-5x", "fas fa-skull-cow fa-5x", "fas fa-skull-cow fa-5x",
+                  "fas fa-fighter-jet fa-5x",
+                  "fas fa-fighter-jet fa-5x", "fas fa-baseball-ball", "fas fa-baseball-ball", "fas fa-anchor fa-5x",
+                  "fas fa-anchor fa-5x", "fas fa-lemon fa-5x", "fas fa-lemon fa-5x", "fas fa-sun fa-5x",
+                  "fas fa-sun fa-5x", "fas fa-industry fa-5x", "fas fa-industry fa-5x"]
+
+        random.shuffle(arr_10)
+        random.shuffle(arr_20)
+
+
+       
+        return render_template("game.html", cards=int(number_of_cards))
 
 
 if __name__ == "__main__":
